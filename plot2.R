baa3eaf06eb82c6192e82d7b934b7f805f5c6480
lvergili <latife.vergili@gmail.com>
2016-04-09 22:13:21 -0700

    Added files via upload

--- plot2.R
+++ plot2.R
@@ -4,7 +4,7 @@ setwd("~/Data_Science/Exploratory_Data_Analaysis/Course_Assigment1")
 ##read data
 data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 ##subset data for specific dates
-
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subSetData$Global_active_power)
